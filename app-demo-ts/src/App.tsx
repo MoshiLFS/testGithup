73a6d6e6e2517b2bc325836bdb57a5d21105909b
dingyu111 <320706773@qq.com>
2019-09-03 10:41:10 +0800

    项目

---
+++ app-demo-ts/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import * as React from 'react';
+
+import './App.css';
+import Header from './component/Header'
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+           <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
